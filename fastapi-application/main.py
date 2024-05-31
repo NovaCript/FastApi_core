8d2238898246017d6f53faed482742539ce0d6d9
NovaCript <glebka.yatsenko@gmail.com>
2024-05-31 21:04:33 +0300

    ready core fastapi

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,20 +1,35 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
-from  core.config import settings
+from core.config import settings
 
 from api import router as api_router
+from core.models import db_helper
 
-app = FastAPI()
-app.include_router(
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
+main_app.include_router(
     api_router,
-    prefix=settings.api.prefix,
 )
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
-        reload=True
+        reload=True,
     )
